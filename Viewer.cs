4eec70d689df46db9307f08449adba5a517dd0e7
Nicolas Tavares <138027100+nicolas-tavares@users.noreply.github.com>
2025-04-01 10:17:05 -0300

    Create Viewer.cs

---
+++ Viewer.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Text.RegularExpressions;
+
+namespace EditorHtml
+{
+    public class Viewer
+    {
+        public static void Show(string text)
+        {
+            Console.Clear();
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.Clear();
+            Console.WriteLine(" MODO VISUALIZAÇÃO");
+            Console.WriteLine("\n =====================");
+            Replace(" " + text);
+            Console.WriteLine("=====================\n\n");
+            Console.WriteLine("* Pressione qualquer tecla para voltar *");
+            Console.ReadKey();
+
+            Menu.Show();
+        }
+
+        public static void Replace(string text)
+        {
+            var strong = new Regex(@"<\s*strong[^>]*>(.*?)<\s*/\s*strong>");
+            var words = text.Split(' ');
+
+            for (var i = 0; i < words.Length; i++)
+            {
+                if (strong.IsMatch(words[i]))
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkBlue;
+                    Console.Write(
+                        words[i].Substring(
+                            words[i].IndexOf('>') + 1,
+                            (
+                                (words[i].LastIndexOf('<') - 1) -
+                                words[i].IndexOf('>'))
+                        )
+                    );
+                    Console.Write(" ");
+
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    Console.Write(words[i]);
+                    Console.Write(" ");
+                }
+            }
+        }
+
+    }
+}
